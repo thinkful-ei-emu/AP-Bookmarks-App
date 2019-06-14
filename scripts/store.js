effd1ca7822a31c9bb26940173d3a0fca0ef42af
Arpan Patel <adp8ke@gmail.com>
2019-06-14 10:01:55 -0400

    added fxn stubs

---
+++ scripts/store.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const store = (function(){
+
+    //adding bookmark function
+
+    function addBookmark(bookmark){
+        // this.bookmarks.push(bookmark);
+
+    }
+
+    //deleting bookmark by id
+
+    function deleteBookmark(id){
+        // this.bookmarks = this.bookmarks.filter(bookmark => bookmark.id !== id);
+    }
+
+    //filter by rating
+
+    function filterByRating(){
+
+    }
+    
+    //expand bookmark
+
+    function expandBookmark(){
+
+    }
+
+    //find by ID
+
+    const findById = function(id){
+        // return this.bookmarks.find(bookmark => bookmark.id === id)
+    }
+
+
+
+
+    return {
+        // bookmarks: [],
+        // adding: false,
+        // description: '',
+        // expanded: false,
+        // findById,
+        // expandBookmark, 
+        // filterByRating, 
+        // deleteBookmark,
+        // addBookmark,
+    };
+
+
+
+}());
